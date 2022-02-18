3f94454ffd76e49cd8da429d5cc57459917a0b7f
Daniel Körtvélyessy <daniel.koertvelyessy@bitpack.io>
2022-02-18 12:15:29 +0100

    initiate working template

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,10 +1,10 @@
 @extends('_layouts.main')
 
+@section('maintitle',$page->title)
+
 @section('body')
 <div class="p-8" x-data="{ open : false}">
     <h1 class="text-3xl font-bold">Hello world!!!!</h1>
-
-
     <button @click="open=!open">Klick</button>
     <div class="p-8">
         <h2 class="text-xl" x-show="open">{{ $page->description }}</h2>
